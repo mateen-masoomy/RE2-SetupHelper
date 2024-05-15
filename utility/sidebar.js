261e7276c9904ffbbf661e2ea25fbafad4e64d4a
mateen-masoomy <103611563+mateen-masoomy@users.noreply.github.com>
2024-05-15 16:28:49 -0400

    CSS fixes

--- utility/sidebar.js
+++ utility/sidebar.js
@@ -1,9 +1,9 @@
 /* Set the width of the side bar to 250px */
 function openSidebar() {
-    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("sidebar").classList.add('sidebar-active');
 }
 
 /* Set the width of the side bar to 0 */
 function closeSidebar() {
-    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("sidebar").classList.remove('sidebar-active');
 }
