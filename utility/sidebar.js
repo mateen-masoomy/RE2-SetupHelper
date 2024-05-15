2477d3e3e7fe6dedab540188778e5456fc8f6964
mateen-masoomy <103611563+mateen-masoomy@users.noreply.github.com>
2024-05-15 13:51:09 -0400

    Added sidebar

---
+++ utility/sidebar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side bar to 250px */
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+/* Set the width of the side bar to 0 */
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+}
